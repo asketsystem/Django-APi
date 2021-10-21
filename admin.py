d38deefb4085601f8335a703c6e8a456e3067027
asketsystem <asketsystem1@gmail.com>
2021-10-21 12:38:55 +0000

    Model Class

---
+++ admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import CarSpecs
+
+
+admin.site.register(CarSpecs)
+
+# Register your models here.
---
+++ models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class CarSpecs(models.Model):
+    car_brand = models.CharField(max_length=50)
+    car_model = models.CharField(max_length=100)
+    production_year = models.CharField(max_length=10)
+    car_body = models.CharField(max_length=100)
+    engine_type = models.CharField(max_length=50) 
